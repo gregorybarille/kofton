d9b0021ef53263ddcc097f3ffc6aa95e8406062f
Jerome Ludmann <jerome.ludmann@gmail.com>
2019-05-28 00:17:46 +0200

    Align paragraph text to left in AppContact

--- src/components/AppContact.vue
+++ src/components/AppContact.vue
@@ -11,9 +11,11 @@
           Kofton Music met à votre disposition une adresse dédiée :<br>
           <a href="mailto:contact@kofton-music.com">contact@kofton-music.com</a><br>
           </p>
+          <div class="text-xs-left">
           <p>Dans la mesure du possible n'envoyez pas un fichier son ou vidéo en pièce jointe de l'email, car il risque de ne pas pouvoir être reçu du fait de sa taille importante (taille maximum pour envoie par email est de 5 Mo, en Mp3 et un titre par email).</p>
           <p>Insérez dans votre email un lien vers un blog ou un site où nous pourrons écouter votre musique (si vous n'avez pas de blog, vous pouvez utiliser Soundcloud ou We Transfer).</p>
           <p>Si votre maquette venait à susciter l'intérêt de notre label, nous ne manquerions pas de vous recontacter. Aucune autre réponse ou information relative à ces maquettes ne sera transmise par téléhpone ou par email.</p>
+          </div>
           </div>
 
           </v-card-text>
