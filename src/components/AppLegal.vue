0f2fb31b1450226ba2c5a2f8eca82c2268d6ba5c
sillegue <sillegue@me.com>
2019-05-27 15:14:00 +0200

    Template added

--- src/components/AppLegal.vue
+++ src/components/AppLegal.vue
@@ -5,15 +5,41 @@
         <v-card>
 
           <v-card-title primary-title>
-            <div>
             <h1>Mentions légales</h1>
-
-            </div>
           </v-card-title>
-        <v-text>
+        <v-card-text class="text-xs-center">
         <v-list>
+        <div>
+        <p><strong>KOFTON MUSIC</strong></p>
+        <p>7 ter rue des hauts trévois - 10000 Troyes - France<br>
+        06 52 28 66 83<br>
+        contact@kofton-music.com<br>
+        www.kofton-music.com</p>
+        <p>Société sous le regime de Auto Entreprise<br>
+        Numéro d'enregistrement au registre du commerce et des sociétés :<br>
+        RCS Troyes 843 815 606<br>
+        Directeur de la publication : Mathieu Tonneau</p>
+        </div>
+        <v-divider></v-divider>
+        <br>
+        <div>
+        <p><strong>Hébergement :</strong></p>
+        <p>1&1 Internet SARL<br>
+        SARL au capital de 100 000 EUR<br>
+        RCS Sarreguemines B 431 303 775<br>
+        SIRET 431 303 775 000 16<br>
+        Code APE: 6201Z<br>
+        Identification intracommunautaire FR 13 431303775<br>
+        Téléphone : 0970 808 911</p>
+        <p>1&1 IONOS SE<br>
+        Elgendorfer Str. 57<br>
+        56410 Montabau<br>
+        Allemagne<br>
+        Téléphone: +49 (0) 721 170 555<br>
+        </p>
+        </div>
         </v-list>
-        </v-text>
+        </v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
