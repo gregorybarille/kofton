be28f093d62c889bd50c800c7ecc71c63d1b2c63
sillegue <sillegue@me.com>
2019-05-29 10:16:10 +0200

    Updated json and script to sort release by dates

--- mise_a_jour.py
+++ mise_a_jour.py
@@ -2,18 +2,22 @@ import html
 import json
 import os
 from pathlib import Path
+from datetime import datetime
 
 
 def read_artistes_directory(artistes_informations):
+    artistes_informations["sortie"] = []
     artiste_directory = './public/assets/Artistes'
     directory_list = Path('./public/assets/Artistes').iterdir()
     for directory in directory_list:
         if directory.name != '.DS_Store':
             artiste_name = directory.name
+            print(artiste_name)
             if Path(f'{directory}/bio.json').is_file:
                 with open(Path(f'{directory}/bio.json'), 'r') as json_file:
                     json_data = json.load(json_file)
                 artistes_informations[artiste_name] = json_data
+                artistes_informations["sortie"].append({"artiste": artiste_name, "date": json_data["sortie"]["date_to_sort"]})
             else:
                 print(f'The bio.json file is missing for {artiste_name}')
             if Path(f'{directory}/bio.html').is_file:
@@ -45,6 +49,12 @@ def read_artistes_directory(artistes_informations):
                 print(f'The images folder is missing for {artiste_name}')
 
 
+def sort_releases_by_dates(artistes_informations):
+    print(artistes_informations["sortie"])
+    artistes_informations["sortie"].sort(key=lambda x: datetime.strptime(x['date'], '%d/%m/%Y'), reverse=True)
+    print(artistes_informations["sortie"])
+
+
 def write_json(artistes_informations):
     with open('./public/data.json', 'w+') as file_json:
         json.dump(artistes_informations, file_json, sort_keys=True)
@@ -53,6 +63,7 @@ def write_json(artistes_informations):
 def main():
     artistes_informations = {}
     read_artistes_directory(artistes_informations)
+    sort_releases_by_dates(artistes_informations)
     write_json(artistes_informations)
 
 
