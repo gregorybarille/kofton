6fd36baff5feb06614b6c1e9c1385aa531246583
Sillegue <greg@babar.io>
2019-05-29 21:53:38 +0200

    updated python script, data and .gitignore

--- mise_a_jour.py
+++ mise_a_jour.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 
 def read_artistes_directory(artistes_informations):
-    artistes_informations["sorties"] = []
     artiste_directory = './public/assets/Artistes'
     directory_list = Path('./public/assets/Artistes').iterdir()
     for directory in directory_list:
@@ -16,33 +15,33 @@ def read_artistes_directory(artistes_informations):
             if Path(f'{directory}/bio.json').is_file:
                 with open(Path(f'{directory}/bio.json'), 'r') as json_file:
                     json_data = json.load(json_file)
-                artistes_informations[artiste_name] = json_data
+                artistes_informations["artistes"][artiste_name] = json_data
                 artistes_informations["sorties"].append({"artiste": artiste_name, "date": json_data["sortie"]["date_to_sort"]})
             else:
                 print(f'The bio.json file is missing for {artiste_name}')
             if Path(f'{directory}/bio.html').is_file:
                 with open(Path(f'{directory}/bio.html')) as html_file:
                     bio_content = html.escape(html_file.read())
-                    artistes_informations[artiste_name]['bio'] = bio_content
+                    artistes_informations["artistes"][artiste_name]['bio'] = bio_content
             else:
                 print(f'The bio.html file is missing for {artiste_name}')
             if Path(f'{directory}/images').is_dir():
                 if Path(f'{directory}/images/artiste.png').is_file():
-                    artistes_informations[artiste_name]['images']['artiste'] = f'assets/Artistes/{artiste_name}/images/artiste.png'
+                    artistes_informations["artistes"][artiste_name]['images']['artiste'] = f'assets/Artistes/{artiste_name}/images/artiste.png'
                 else:
                     print(f'The artiste.png file is missing for {artiste_name}')
-                    artistes_informations[artiste_name]['images']['artiste'] = f'assets/Artistes/{artiste_name}/images/bio.png'
+                    artistes_informations["artistes"][artiste_name]['images']['artiste'] = f'assets/Artistes/{artiste_name}/images/bio.png'
                 if Path(f'{directory}/images/bio.png').is_file():
-                    artistes_informations[artiste_name]['images']['bio'] = f'assets/Artistes/{artiste_name}/images/bio.png'
+                    artistes_informations["artistes"][artiste_name]['images']['bio'] = f'assets/Artistes/{artiste_name}/images/bio.png'
                 else:
                     print(f'The bio.png file is missing for {artiste_name}')
-                    artistes_informations[artiste_name]['images']['bio'] = f'assets/Artistes/{artiste_name}/images/artiste.png'
+                    artistes_informations["artistes"][artiste_name]['images']['bio'] = f'assets/Artistes/{artiste_name}/images/artiste.png'
                 if Path(f'{directory}/images/name.png').is_file():
-                    artistes_informations[artiste_name]['images']['name'] = f'assets/Artistes/{artiste_name}/images/name.png'
+                    artistes_informations["artistes"][artiste_name]['images']['name'] = f'assets/Artistes/{artiste_name}/images/name.png'
                 else:
                     print(f'The artiste.png file is missing for {artiste_name}')
                 if Path(f'{directory}/images/titre.png').is_file():
-                    artistes_informations[artiste_name]['images']['titre'] = f'assets/Artistes/{artiste_name}/images/titre.png'
+                    artistes_informations["artistes"][artiste_name]['images']['titre'] = f'assets/Artistes/{artiste_name}/images/titre.png'
                 else:
                     print(f'The titre.png file is missing for {artiste_name}')
             else:
@@ -61,7 +60,7 @@ def write_json(artistes_informations):
 
 
 def main():
-    artistes_informations = {}
+    artistes_informations = {"artistes": {}, "sorties": []}
     read_artistes_directory(artistes_informations)
     sort_releases_by_dates(artistes_informations)
     write_json(artistes_informations)
