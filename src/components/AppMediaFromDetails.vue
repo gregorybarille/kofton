32292987a7a3d7960c73aa3c257bff16e8b5c971
sillegue <sillegue@me.com>
2019-06-04 15:55:15 +0200

    updated layout for title and media

--- src/components/AppMediaFromDetails.vue
+++ src/components/AppMediaFromDetails.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>Titre</div>
+  <v-layout>
+    <v-flex xs10 sm6>
+      <v-img aspect-ratio="1" contain :src="`/${artist.images.titre}`"></v-img>
+      <v-divider light></v-divider>
+    </v-flex>
+    <v-flex xs2 sm6>
+      <v-layout column align-center justify-center fill-height>
+        <v-btn flat small :href="value" target="_blank">
+          <v-icon>fa facebook</v-icon>
+        </v-btn>
+        <v-btn flat small :href="value" target="_blank">
+          <v-icon>fa facebook</v-icon>
+        </v-btn>
+        <v-btn flat small :href="value" target="_blank">
+          <v-icon>fa facebook</v-icon>
+        </v-btn>
+        <v-btn flat small :href="value" target="_blank">
+          <v-icon>fa facebook</v-icon>
+        </v-btn>
+      </v-layout>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
   props: {
-    jsonData: {
+    artist: {
       type: Object
     },
     name: {
