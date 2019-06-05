0bd1a7e9c2a53ca0b0807c1833afa4bebd6687eb
sillegue <sillegue@me.com>
2019-06-05 18:04:07 +0200

    Added v-if for HezCut

--- src/components/AppMedia.vue
+++ src/components/AppMedia.vue
@@ -8,31 +8,29 @@
               <v-card :to="{path: `/artistes/${artiste}/media`}">
                 <v-img aspect-ratio="1.5" contain :src="jsonData.artistes[artiste].images.titre"></v-img>
                 <v-divider light></v-divider>
-                <v-card-actions>
-                  <v-layout>
-                    <v-flex xs12 sm8 offset-sm2 v-if="artiste == 'HezCut'">
-                      <iframe
-                        scrolling="no"
-                        frameborder="0"
-                        allowtransparency="true"
-                        src="https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=ff0000&layout=&size=medium&type=tracks&id=660748242&app_id=1"
-                        width="100%"
-                        height="166"
-                      ></iframe>
-                    </v-flex>
+                <v-layout>
+                  <v-flex xs12 sm8 offset-sm2 v-if="artiste == 'HezCut'">
+                    <iframe
+                      scrolling="no"
+                      frameborder="0"
+                      allowtransparency="true"
+                      src="https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=ff0000&layout=&size=medium&type=tracks&id=660748242&app_id=1"
+                      width="100%"
+                      height="166"
+                    ></iframe>
+                  </v-flex>
 
-                    <v-flex xs12 sm8 offset-sm2 v-else>
-                      <iframe
-                        width="100%"
-                        height="166"
-                        scrolling="no"
-                        frameborder="no"
-                        allow="autoplay"
-                        :src="`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${jsonData.artistes[artiste].sortie.platform.soundcloud}&color=%23007eff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true`"
-                      ></iframe>
-                    </v-flex>
-                  </v-layout>
-                </v-card-actions>
+                  <v-flex xs12 sm8 offset-sm2 v-else>
+                    <iframe
+                      width="100%"
+                      height="166"
+                      scrolling="no"
+                      frameborder="no"
+                      allow="autoplay"
+                      :src="`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${jsonData.artistes[artiste].sortie.platform.soundcloud}&color=%23007eff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true`"
+                    ></iframe>
+                  </v-flex>
+                </v-layout>
               </v-card>
             </v-flex>
           </v-layout>
