dad468eacbc8ec0926145071730e4c3c462ac6fc
sillegue <sillegue@me.com>
2019-06-05 17:00:13 +0200

    Added youtube video to artist details

--- src/components/AppMediaFromDetails.vue
+++ src/components/AppMediaFromDetails.vue
@@ -4,7 +4,7 @@
       <v-card flat>
         <v-container fluid grid-list-md>
           <v-layout column nowrap>
-            <v-flex>
+            <v-flex v-if="name == 'HezCut'">
               <iframe
                 scrolling="no"
                 frameborder="0"
@@ -12,6 +12,27 @@
                 :src="`https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=ff0000&layout=dark&size=medium&type=${artist.sortie.platform.deezer}&app_id=1`"
                 width="600"
                 height="600"
+              ></iframe>
+            </v-flex>
+            <v-flex v-else>
+              <iframe
+                width="600"
+                height="600"
+                scrolling="no"
+                frameborder="no"
+                allow="autoplay"
+                :src="`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${artist.sortie.platform.soundcloud}&color=%23007eff&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`"
+              ></iframe>
+            </v-flex>
+            <v-divider></v-divider>
+            <v-flex>
+              <iframe
+                width="600"
+                height="400"
+                :src="`https://www.youtube-nocookie.com/embed/${artist.sortie.platform.youtube}`"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
               ></iframe>
             </v-flex>
           </v-layout>
